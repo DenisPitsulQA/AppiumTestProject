de86d1d91cfccf68e39f55d8e7048e335f7ff6af
Denys Pitsul <denys.pitsul.qa@gmail.com>
2023-06-29 15:33:41 +0300

    removed unneeded imports

--- src/main/java/org/example/SignInPage.java
+++ src/main/java/org/example/SignInPage.java
@@ -1,13 +1,10 @@
 package org.example;
 
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriver;
 
 public class SignInPage {
 
