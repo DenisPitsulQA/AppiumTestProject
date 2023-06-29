b42d0adda0b6c52e776a0f48dd93141f9c91c9dd
Denys Pitsul <denys.pitsul.qa@gmail.com>
2023-06-29 15:03:01 +0300

    Added openAppOnRemoteDevice

--- src/main/java/org/example/SignInPage.java
+++ src/main/java/org/example/SignInPage.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 
 public class SignInPage {
 
-    private WebDriver driver;
+    private AndroidDriver driver;
 
     private By emailInput = By.xpath("//android.view.View[@content-desc='Sign in']/android.widget.EditText[1]");
     private By passwordInput = By.xpath("//android.view.View[@content-desc='Sign in']/android.widget.EditText[2]");
@@ -13,21 +19,24 @@ public class SignInPage {
     private By accountDoesNotExistMessage = By.xpath("//android.view.View[@content-desc=\"Sign in\n" +
             "Account doesn't exist\"]");
 
-    public SignInPage(WebDriver driver) {
+    public SignInPage(AndroidDriver driver) {
         this.driver = driver;
     }
 
     public void typeEmail(String email) {
         driver.findElement(emailInput).click();
         driver.findElement(emailInput).sendKeys(email);
+        driver.hideKeyboard();
     }
 
     public void typePassword(String password) {
         driver.findElement(passwordInput).click();
         driver.findElement(passwordInput).sendKeys(password);
+        driver.hideKeyboard();
     }
 
     public void clickSignIn() {
+        scrollDown();
         driver.findElement(signInBtn).click();
     }
 
@@ -39,5 +48,18 @@ public class SignInPage {
 
     public boolean isAccountDoesNotExistMessageVisible() {
         return driver.findElements(accountDoesNotExistMessage).size() > 0;
+    }
+
+    public void scrollDown() {
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int width = size.width;
+        int height = size.height;
+        int middleOfX = width / 2;
+        int startYCoordinate = (int) (height * 0.8);
+        int endYCoordinate = (int) (height * 0.2);
+        action.press(PointOption.point(middleOfX, startYCoordinate))
+                .waitAction()
+                .moveTo(PointOption.point(middleOfX, endYCoordinate)).release().perform();
     }
 }
--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.net.MalformedURLException;
@@ -14,10 +16,16 @@ public class BaseTest {
 
     @BeforeClass
     public void setUp() {
-        openApp();
+        openAppOnRemoteDevice();
     }
 
-    public void openApp() {
+    @AfterClass
+    public void tearDown() {
+        if (driver != null)
+            driver.quit();
+    }
+
+    public void openAppOnLocalDevice() {
         String dir = System.getProperty("user.dir");
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability("appium:deviceName", "Redmi 9");
@@ -37,6 +45,31 @@ public class BaseTest {
             throw new RuntimeException(e);
         }
         driver = new AndroidDriver(url, cap);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public void openAppOnRemoteDevice() {
+        MutableCapabilities caps = new MutableCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("appium:app", "storage:filename=GoNo.apk");  // The filename of the mobile app
+        caps.setCapability("appium:deviceName", "Android GoogleAPI Emulator");
+        caps.setCapability("appium:deviceOrientation", "portrait");
+        caps.setCapability("appium:platformVersion", "12");
+        caps.setCapability("appium:automationName", "UiAutomator2");
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("username", "oauth-denys.pitsul.qa-94d0a");
+        sauceOptions.setCapability("accessKey", "14194a4f-c9ea-4143-b8b7-a9d7a00013e8");
+        sauceOptions.setCapability("build", "appium-build-CJZGF");
+        sauceOptions.setCapability("name", "Sign In");
+        caps.setCapability("sauce:options", sauceOptions);
+
+        URL url = null;
+        try {
+            url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        driver = new AndroidDriver(url, caps);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 }
